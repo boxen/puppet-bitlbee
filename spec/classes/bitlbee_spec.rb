3cb68de0e304345e57c43edcf89bf5ed65923cbe
Larry Glenn <yrral.glenn@gmail.com>
2013-04-23 16:51:31 -0400

    Only listen on localhost interface

--- spec/classes/bitlbee_spec.rb
+++ spec/classes/bitlbee_spec.rb
@@ -12,8 +12,9 @@ describe 'bitlbee' do
   it { should contain_package('bitlbee').with_provider('homebrew') }
   it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with( 'group' => 'wheel', 'owner' => 'root') }
   it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<string>-p<\/string>\s*<string>6667<\/string>/) }
+  it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<string>-i<\/string>\s*<string>localhost<\/string>/), "Should be running with -i localhost." }
   it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<key>UserName<\/key>\s*<string>#{facts[:luser]}<\/string>/) }
   it { should contain_file("#{facts[:boxen_home]}/config/bitlbee").with('ensure' => 'directory') }
   it { should contain_file("#{facts[:boxen_home]}/log/bitlbee").with('ensure' => 'directory') }
   it { should contain_service('bitlbee').with(:ensure => 'running') }
-  end
+end
