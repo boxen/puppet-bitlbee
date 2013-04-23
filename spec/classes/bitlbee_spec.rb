2ba8676d04d473e7d196d5157d64b831dae0fb6c
Larry Glenn <yrral.glenn@gmail.com>
2013-04-23 16:52:24 -0400

    Add helpful message for port spec.

--- spec/classes/bitlbee_spec.rb
+++ spec/classes/bitlbee_spec.rb
@@ -11,7 +11,7 @@ describe 'bitlbee' do
   it { should include_class('bitlbee::config') }
   it { should contain_package('bitlbee').with_provider('homebrew') }
   it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with( 'group' => 'wheel', 'owner' => 'root') }
-  it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<string>-p<\/string>\s*<string>6667<\/string>/) }
+  it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<string>-p<\/string>\s*<string>6667<\/string>/), "Should run on port 6667" }
   it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<string>-i<\/string>\s*<string>localhost<\/string>/), "Should be running with -i localhost." }
   it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<key>UserName<\/key>\s*<string>#{facts[:luser]}<\/string>/) }
   it { should contain_file("#{facts[:boxen_home]}/config/bitlbee").with('ensure' => 'directory') }
