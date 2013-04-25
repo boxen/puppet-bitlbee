5bb0465be129c4f03280a5d35f4322f921f47711
Larry Glenn <yrral.glenn@gmail.com>
2013-04-25 12:33:43 -0400

    Add spec for -D switch

--- spec/classes/bitlbee_spec.rb
+++ spec/classes/bitlbee_spec.rb
@@ -13,6 +13,7 @@ describe 'bitlbee' do
   it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with( 'group' => 'wheel', 'owner' => 'root') }
   describe "bitlbee.plist" do
     it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<key>ProgramArguments<\/key>\s*<array>\s*<string>#{facts[:boxen_home]}\/homebrew\/sbin\/bitlbee<\/string>/), "Executable should be /opt/boxen/homebrew/sbin/bitlbee" }
+    it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<string>-D<\/string>/), "Should run in daemon mode" }
     it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<string>-p<\/string>\s*<string>6667<\/string>/), "Should run on port 6667" }
     it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<string>-i<\/string>\s*<string>localhost<\/string>/), "Should be running with -i localhost." }
     it { should contain_file('/Library/LaunchDaemons/bitlbee.plist').with_content(/<key>UserName<\/key>\s*<string>#{facts[:luser]}<\/string>/) }
